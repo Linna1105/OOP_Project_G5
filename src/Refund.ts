2a66bf9f1e7e9b01a2b59bda7a57ae9f32f6033e
That-VEN <thattven@gmail.com>
2025-05-30 08:07:28 +0700

    feat-admin/refund

---
+++ src/Refund.ts
@@ -0,0 +1,19 @@
+import {  } from "./";
+export class Refund{
+    private refundId :number;
+    private orderItem :OrderItem;
+    private amount : number;
+    private refundDate: Date;
+    private status: string;
+    
+    constructor(refundId: number, orderItem: OrderItem, amount: number, refundDate: Date, status: string) {
+        this.refundId = refundId;
+        this.orderItem = orderItem;
+        this.amount = amount;
+        this.refundDate = refundDate;
+        this.status = status;
+    }
+
+
+
+}

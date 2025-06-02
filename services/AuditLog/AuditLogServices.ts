063677c06bbf9b9c06e4e08098726c1919348976
Linna1105 <linna.rin@student.passerellesnumeriques.org>
2025-06-02 09:05:20 +0700

    Auditing

---
+++ services/AuditLog/AuditLogServices.ts
@@ -0,0 +1,8 @@
+export class AuditLog {
+    recordAction(user:UserActivation,action:string,target:string):void{
+        
+    }
+    getAuditLogs(user:UserActivation,action:string,target:string):void{
+
+    }
+}
